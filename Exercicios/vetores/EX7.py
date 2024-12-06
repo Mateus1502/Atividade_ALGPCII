7fb7d2cb88d760092f659c1a3506bea0f645ce0d
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2024-12-06 09:13:21 -0300

    Update EX7.py

--- Exercicios/vetores/EX7.py
+++ Exercicios/vetores/EX7.py
@@ -1,17 +1,9 @@
-"""
-Ler 100 números de matriculas de alunos e armazenar em um vetor. Esses números são distintos, ou seja, 
-não existem números de matriculas iguais. Caso o usuário informa um número de matrículo que já existe, 
-o programa deverá emitir um alerta.
-"""
-
 matricula = []
 
-for c in range(100):
-    num = int(input('Digite matricula: '))
-    #vai percorrer se o numero digitado já existe no vetor
-    if num in matricula:
+for i in range(100):
+    numero = int(input('Digite matricula: '))
+    if numero in matricula:
         print('Esse valor de matricula já existe, digite outro numero')
     else:
-        matricula.append(num)
-
+        matricula.append(numero)
 print(matricula)
