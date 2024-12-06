62c3ad4b1984f6b80574e8bb6c86299b42fb1731
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2024-12-06 08:07:25 -0300

    Update EX3.py

--- Exercicios/vetores/EX3.py
+++ Exercicios/vetores/EX3.py
@@ -1,17 +1,13 @@
-"""
-Faça um programa que some o conteúdo de dois vetores e armazene o resultado em um terceiro vetor.
-"""
-
-Vet1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
-Vet2 = [2, 4, 6, 8, 10, 12, 14, 16]
-Vet3 = []
+vetor1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+vetor2 = [2, 4, 6, 8, 10, 12, 14, 16]
+vetor3 = []
 i = 0
-while i < len(Vet1) or i < len(Vet2):                #vai rodar enquanto houver elementos em alguns dos vetores
-    if i <len(Vet1) and i < len(Vet2):                  #verifica se tem o índice i em ambos os vetores
-        Vet3.append(Vet1[i] + Vet2[i])
-    elif i < len(Vet1):                               #vê se só no Vet1 tem
-        Vet3.append(Vet1[i])                           
+while i < len(vetor1) or i < len(vetor2):              
+    if i <len(vetor1) and i < len(vetor2):                 
+        vetor3.append(vetor1[i] + vetor2[i])
+    elif i < len(vetor1):                             
+        vetor3.append(vetor1[i])                           
     else:
-        Vet3.append(Vet2[i])                            #Se não tiver no Vet1, ele vai para o else e adiciona o elemento do Vet2
+        vetor3.append(vetor2[i])                           
     i += 1
-print(Vet3)
+print(vetor3)
