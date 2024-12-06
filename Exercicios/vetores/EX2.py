8e4d740326574e3ebe9fc92eae65cb559a2c22fe
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2024-12-06 08:04:31 -0300

    Update EX2.py

--- Exercicios/vetores/EX2.py
+++ Exercicios/vetores/EX2.py
@@ -1,13 +1,8 @@
-"""
-Faça um programa que copie o conteúdo de um vetor em um segundo vetor.
-"""
-
-vet = [1, 2, 56, 92, 100]
+vetor = [1, 2, 56, 92, 100]
 aux = []
 
-for c in vet:
-    aux.append(c)
-
+for i in vetor:
+    aux.append(i)
 print(vet)
 print(aux)
 
