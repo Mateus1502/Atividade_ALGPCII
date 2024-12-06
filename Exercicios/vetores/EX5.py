d6ff0094bccf4dee553167edfdfe4576acdc6787
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2024-12-06 09:12:05 -0300

    Update EX5.py

--- Exercicios/vetores/EX5.py
+++ Exercicios/vetores/EX5.py
@@ -1,18 +1,9 @@
-"""
-Faça um programa que defina dois vetores A = [2, 4, 7, 13, 14, 15, 16] e B = [1, 6, 7, 11, 13, 16, 18] e faça as seguintes 
-operações de conjuntos:
-
-A ⋃ B: União (todos os valores de ambos os vetores)
-A ⋂ B: Intersecção (apenas valores que existam em ambos)
-A − B: Diferença (apenas valores que não apareçam simultaneamente em ambos conjuntos)
-"""
-
 A = [2, 4, 7, 13, 14, 15, 16]
 B = [1, 6, 7, 11, 13, 16, 18]
 C = []
 D = []
 
-#uniao
+
 uniao = []
 for a in A:
     if a not in uniao:
@@ -22,16 +13,14 @@ for b in B:
         uniao.append(b)
 print(uniao)
 
-#intersecção
+
 for a in A:
-    if a in B:                  #verifica se o elemento A está em B
+    if a in B:                 
         C.append(a)
 print(C)
 
-#Diferença
+
 for a in A:
-    if a not in B:              #verifica se o elemento A não está em B
+    if a not in B:              
         D.append(a)
 print(D)
-
-
